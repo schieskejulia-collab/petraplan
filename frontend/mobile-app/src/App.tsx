1e675c174701b48a1a3518ed53c0cb5a39d1f612
schieskejulia-collab <schieskejulia@gmail.com>
2026-07-15 09:22:06 +0200

    App.tsx aktualisieren

--- frontend/mobile-app/src/App.tsx
+++ frontend/mobile-app/src/App.tsx
@@ -1,11 +1,11 @@
-import { Route, Switch } from "wouter"
+import { Route, Switch } from "wouter";
 
-import Home from "./pages/Home"
-import Upload from "./pages/Upload"
-import AnalyzePage from "./pages/AnalyzePage"
-import HistoryPage from "./pages/HistoryPage"
-import ResultsPage from "./pages/results"
-import NotFound from "./pages/not-found"
+import Home from "./pages/Home";
+import Upload from "./pages/Upload";
+import AnalyzePage from "./pages/AnalyzePage";
+import HistoryPage from "./pages/HistoryPage";
+import ResultsPage from "./pages/results";
+import NotFound from "./pages/not-found";
 
 export default function App() {
   return (
@@ -17,5 +17,5 @@ export default function App() {
       <Route path="/results" component={ResultsPage} />
       <Route component={NotFound} />
     </Switch>
-  )
+  );
 }
