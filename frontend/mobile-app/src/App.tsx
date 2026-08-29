23dac9631a749541f1329803a774f85f05f30437
schieskejulia-collab <schieskejulia@gmail.com>
2026-08-29 13:16:57 +0200

    Make PetraPlan trace app canonical mobile entry

--- frontend/mobile-app/src/App.tsx
+++ frontend/mobile-app/src/App.tsx
@@ -1,25 +1,14 @@
-import { Route, Switch } from "wouter";
-
-import Home from "./pages/Home";
-import Upload from "./pages/Upload";
-import AnalyzePage from "./pages/AnalyzePage";
-import HistoryPage from "./pages/HistoryPage";
+import { Redirect, Route, Switch } from "wouter";
 import CasesPage from "./pages/CasesPage";
 import CaseDetailPage from "./pages/CaseDetailPage";
-import ResultsPage from "./pages/results";
-import NotFound from "./pages/not-found";
 
 export default function App() {
   return (
     <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/upload" component={Upload} />
-      <Route path="/analyse" component={AnalyzePage} />
-      <Route path="/history" component={HistoryPage} />
+      <Route path="/" component={() => <Redirect to="/cases" />} />
       <Route path="/cases" component={CasesPage} />
       <Route path="/cases/:caseId" component={CaseDetailPage} />
-      <Route path="/results" component={ResultsPage} />
-      <Route component={NotFound} />
+      <Route component={() => <Redirect to="/cases" />} />
     </Switch>
   );
 }
