3c983f47598500e1bc1f9d5998102e793480ff66
schieskejulia-collab <schieskejulia@gmail.com>
2026-08-29 19:15:40 +0200

    Fix ESM import extension for Vercel case detail API

--- api/cases/[recordId].ts
+++ api/cases/[recordId].ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-import { getCaseTrace } from '../../api-server/src/services/caseTrace';
+import { getCaseTrace } from '../../api-server/src/services/caseTrace.js';
 
 const UUID_RE = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
