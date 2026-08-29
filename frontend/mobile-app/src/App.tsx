9c43a5435786da335e7d31a8fcaa459919bd43d6
schieskejulia-collab <schieskejulia@gmail.com>
2026-08-29 05:27:41 +0200

    Isolate N64 review surface from broken legacy routes

--- frontend/mobile-app/src/App.tsx
+++ frontend/mobile-app/src/App.tsx
@@ -1,23 +1,12 @@
 import { Route, Switch } from "wouter";
-
-import Home from "./pages/Home";
-import Upload from "./pages/Upload";
-import AnalyzePage from "./pages/AnalyzePage";
-import HistoryPage from "./pages/HistoryPage";
 import IngestionPage from "./pages/IngestionPage";
-import ResultsPage from "./pages/results";
-import NotFound from "./pages/not-found";
 
 export default function App() {
   return (
     <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/upload" component={Upload} />
-      <Route path="/analyse" component={AnalyzePage} />
-      <Route path="/history" component={HistoryPage} />
+      <Route path="/" component={IngestionPage} />
       <Route path="/ingestions" component={IngestionPage} />
-      <Route path="/results" component={ResultsPage} />
-      <Route component={NotFound} />
+      <Route component={IngestionPage} />
     </Switch>
   );
 }
