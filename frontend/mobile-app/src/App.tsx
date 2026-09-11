c4e3cda93336e95abc89c04c84f85ed4234c6e58
schieskejulia-collab <schieskejulia@gmail.com>
2026-09-11 22:36:49 +0200

    feat(ui): route für einheitlichen Nachweisweg

--- frontend/mobile-app/src/App.tsx
+++ frontend/mobile-app/src/App.tsx
@@ -2,14 +2,16 @@ import { Redirect, Route, Switch } from "wouter";
 import CasesPage from "./pages/CasesPage";
 import CaseDetailPage from "./pages/CaseDetailPage";
 import BridgePage from "./pages/BridgePage";
+import UnifiedTracePage from "./pages/UnifiedTracePage";
 
 export default function App() {
   return (
     <Switch>
       <Route path="/" component={() => <Redirect to="/cases" />} />
-          <Route path="/cases" component={CasesPage} />
-          <Route path="/cases/:caseId" component={CaseDetailPage} />
-          <Route path="/bridge" component={BridgePage} />
+      <Route path="/cases" component={CasesPage} />
+      <Route path="/cases/:caseId" component={CaseDetailPage} />
+      <Route path="/bridge" component={BridgePage} />
+      <Route path="/trace" component={UnifiedTracePage} />
       <Route component={() => <Redirect to="/cases" />} />
     </Switch>
   );
